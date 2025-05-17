fc4ba3a3d182e3365c1ee9fb30ffca36ba182093
Sargis Sargsyan <sargis.sargsyan@live.com>
2025-05-17 11:26:32 +0400

    homework 4 from Anna

--- src/main/java/am/picsartacademy/lesson2/Homework3.java
+++ src/main/java/am/picsartacademy/lesson2/Homework3.java
@@ -2,7 +2,26 @@ package am.picsartacademy.lesson2;
 
 public class Homework3 {
     public static void main(String[] args) {
-        int n = 7;
+        int v = 9;
+        for (int j = v; j > 0; j--) {
+            if (j % 2 != 0) {
+                for (int k = 0; k < (v - j) / 2; k++) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k < j; k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+        }
+
+
+
+
+
+
+        int n = 9;
         for (int i = 0; i < n; i++) {
             if (i % 2 != 0) {
                 for (int j = 0; j < (n - i) / 2; j++) {
