d9ee55c79073c3925cf7ffee45b4c5e28e48b2a9
Sargis Sargsyan <sargis.sargsyan@live.com>
2025-05-29 13:06:10 +0400

    Exceptions, try-catch

--- src/main/java/am/picsartacademy/oop/Main.java
+++ src/main/java/am/picsartacademy/oop/Main.java
@@ -11,44 +11,64 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-        int n = 6/0;}
-        catch (ArithmeticException e) {
-            System.out.println("you cannot divide by zero");
 
-        }
         Scanner scanner = new Scanner(System.in);
-        System.out.println("what is your name?");
-        String firstName = scanner.nextLine();
-
-        Human human1 = new Human();
+        int x = 0;
+        int y = 0;
         try {
-            human1.setFirstName(firstName);
-        }
-
-        catch (InvalidNameException e) {
-            System.out.println(e.toString());
-            System.out.println("Setting default name John");
-            human1.setFirstName("John");
-        }
-
-        System.out.println("what is your age?");
-        int age = 0;
-        try {
-            age = scanner.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("The age should be an integer");
-        }
-        try {
-            human1.setAge(age);
-            System.out.println(human1.age);
-        } catch (AgeException e) {
-            System.out.println(e.toString());
-            System.out.println("Defaulting to age 20");
-            human1.setAge(20);
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+        } catch (InputMismatchException exception) {
+            System.out.println(exception.getClass().toString());
         } finally {
-            System.out.println("Finally the age is " + human1.age);
+            scanner.close();
         }
-        System.out.println(human1.age);
+        try {
+            int z = x / y;
+            System.out.print(z);
+        } catch (ArithmeticException exception) {
+            System.out.print(exception.getClass().toString());
+            System.out.println(": / by zero");
+
+        }
+//
+//
+//        try {
+//            int n = 6 / 0;
+//        } catch (ArithmeticException e) {
+//            System.out.println("you cannot divide by zero");
+//
+//        }
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("what is your name?");
+//        String firstName = scanner.nextLine();
+//
+//        Human human1 = new Human();
+//        try {
+//            human1.setFirstName(firstName);
+//        } catch (InvalidNameException e) {
+//            System.out.println(e.toString());
+//            System.out.println("Setting default name John");
+//            human1.setFirstName("John");
+//        }
+//
+//        System.out.println("what is your age?");
+//        int age = 0;
+//        try {
+//            age = scanner.nextInt();
+//        } catch (InputMismatchException e) {
+//            System.out.println("The age should be an integer");
+//        }
+//        try {
+//            human1.setAge(age);
+//            System.out.println(human1.age);
+//        } catch (AgeException e) {
+//            System.out.println(e.toString());
+//            System.out.println("Defaulting to age 20");
+//            human1.setAge(20);
+//        } finally {
+//            System.out.println("Finally the age is " + human1.age);
+//        }
+//        System.out.println(human1.age);
     }
 }
